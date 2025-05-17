c568746047cc90d4b9e419e75c1a2d32de86a53d
athena272 <guilhermera272@gmail.com>
2025-05-17 10:24:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,11 @@ $client = new Client(['verify' => false, 'base_uri' => 'https://www.alura.com.br
 $crawler = new Crawler();
 
 $fetcher = new CourseFetcher($client, $crawler);
-$courses = $fetcher->getCourses('/cursos-online-programacao/php');
+try {
+    $courses = $fetcher->getCourses('/cursos-online-programacao/php');
+} catch (\GuzzleHttp\Exception\GuzzleException $e) {
+    echo 'Error: ' . $e->getMessage();
+}
 
 foreach ($courses as $course) {
     showMessage($course);
