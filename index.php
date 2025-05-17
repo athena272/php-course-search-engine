c89bfaf0d884b18e029a65c4e86c811a84a40356
athena272 <guilhermera272@gmail.com>
2025-05-16 22:08:00 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 
 require 'vendor/autoload.php';
-require 'src/Models/CourseFetcher.php';
 
-use CourseSearchEngine\Models\CourseFetcher;
+use Athena272\CourseSearchEngine\Models\CourseFetcher;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
@@ -14,5 +13,5 @@ $fetcher = new CourseFetcher($client, $crawler);
 $courses = $fetcher->getCourses('/cursos-online-programacao/php');
 
 foreach ($courses as $course) {
-    echo $course . PHP_EOL;
+    showMessage($course);
 }
